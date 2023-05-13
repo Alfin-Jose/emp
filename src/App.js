1dad2f058f57a5573e31e57a0d563c92e68181c9
unknown <evinbabu003@gmail.com>
2023-05-13 17:37:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from'./pages/home'
+import Form from'./pages/form'
+import Front from'./pages/front'
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg' >
+      <Navbar/>
+      <br />
+      <Routes>
+          <Route path="/" element={<Front />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+      </Routes>
+      <br />
     </div>
   );
 }
